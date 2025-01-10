d24808cb2208c3bba3670cf1617ce6933d515db2
dariusbrinzan <brinzandarius27@gmail.com>
2025-01-10 21:24:10 +0200

    finished

--- lab-04-entity-hierarchy/src/main/java/edu/jpa/TABLE_PER_SUBCLASS/Launcher.java
+++ lab-04-entity-hierarchy/src/main/java/edu/jpa/TABLE_PER_SUBCLASS/Launcher.java
@@ -14,33 +14,31 @@ public class Launcher {
         PropertyConfigurator.configure(Launcher.class.getResource("/log4j.properties"));
     }
 
-
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistenceUnits.lab04.TABLE_PER_SUBCLASS");
-        init(emf.createEntityManager());
-        sample(emf.createEntityManager());
-		
-		emf.close();
-    }
+        EntityManager em = emf.createEntityManager();
 
+        init(em);
+        sample(em);
+
+        em.close();
+        emf.close();
+    }
 
     private static void init(EntityManager em) {
         em.getTransaction().begin();
 
         Customer customer = new Customer();
-        customer.setId(1);
         customer.setName("Customer #1 (10% discount).");
         customer.setDiscount(10.0);
         em.persist(customer);
 
         Employee employee = new Employee();
-        employee.setId(2);
         employee.setName("Employee #1.");
         employee.setSalary(1000.0);
         em.persist(employee);
 
         Executive executive = new Executive();
-        executive.setId(3);
         executive.setName("Executive #1.");
         executive.setSalary(2000.0);
         executive.setBonus(30.0);
@@ -49,13 +47,16 @@ public class Launcher {
         em.getTransaction().commit();
     }
 
-
     private static void sample(EntityManager em) {
         em.getTransaction().begin();
 
         Executive executive = em.find(Executive.class, 3);
-        System.out.println(executive.getName());
+        if (executive != null) {
+            System.out.println("Found Executive: " + executive.getName());
+        } else {
+            System.out.println("Executive not found.");
+        }
 
-        em.getTransaction().rollback();
+        em.getTransaction().commit();
     }
 }
